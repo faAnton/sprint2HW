8eac4eebe64640133a3f9a7d5a5dbbdbe3bbbe5c
faAnton <tonifedar@mail.ru>
2026-08-29 21:54:18 +0300

    fixed date formatting

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -33,7 +33,7 @@ function Clock() {
     }
 
     const stringTime = `${date.toLocaleString("ru", {hour: '2-digit', minute: '2-digit', second: '2-digit'})}` || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = `${date.toLocaleString("ru", {day: '2-digit', month: '2-digit', year: '2-digit'})}` || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    const stringDate = `${date.toLocaleString("ru", {day: '2-digit', month: '2-digit', year: 'numeric'})}` || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
     
